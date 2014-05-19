34c8f96909e07c995666f53286dcd7a8a89aa13d
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-19 17:36:40 +0900

    init 'build'

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ Modinit.prototype.template = function (prompt, modinitrc, options) {
   var _package = _.template(fs.readFileSync('template/_package.json').toString());
   var license = _.template(fs.readFileSync('template/LICENSE').toString());
   var cmd = _.template(fs.readFileSync('template/cmd.js').toString());
+  var _index = fs.readFileSync('template/_index.js').toString();
+  var travis = fs.readFileSync('template/travis.yml').toString();
+  var test = fs.readFileSync('template/test.js').toString();
 
   var moduleName = _.slugify(prompt.moduleName);
   var moduleVarName = _.camelize(prompt.moduleName);
@@ -55,9 +58,62 @@ Modinit.prototype.template = function (prompt, modinitrc, options) {
 
   var res = {};
   res.readme = readme;
+  res.readmeStyle = prompt.readme;
   res._package = _package;
   res.license = license;
   res.cmd = cmd;
+  res._index = _index;
+  res.test = test;
+  res.moduleName = moduleName;
+  res.moduleVarName = moduleVarName;
+  if (modinitrc.travis) res.travis = travis;
 
   return res;
+};
+
+Modinit.prototype.build = function (templates) {
+  mkdirp(templates.moduleName, function (err) {
+    if (err) throw err;
+  });
+
+  mkdirp(templates.moduleName + '/bin', function (err) {
+    if (err) throw err;
+  });
+
+  mkdirp(templates.moduleName + '/test', function (err) {
+    if (err) throw err;
+  });
+
+
+  fs.writeFile(moduleName + '/index.js', templates._index, function (err) {
+    if (err) throw err;
+  });
+
+  fs.writeFile(moduleName + '/bin' + modulename, templates.cmd, function (err) {
+    if (err) throw err;
+  });
+
+  fs.writeFile(moduleName + '/test/test.js', templates.test, function (err) {
+    if (err) throw err;
+  });
+
+  fs.writeFile(moduleName + '/' + templates.readmeStyle, templates.readme, function (err) {
+    if (err) throw err;
+  });
+
+  fs.writeFile(moduleName + '/LICENSE', templates.license, function (err) {
+    if (err) throw err;
+  });
+
+  fs.writeFile(moduleName + '/package.json', templates._package, function (err) {
+    if (err) throw err;
+  });
+
+  if (templates.travis) {
+    fs.writeFile(moduleName + 'index.js', templates._index, function (err) {
+      if (err) throw err;
+    });
+  }
+
+  return true;
 };
