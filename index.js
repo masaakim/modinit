44ed12e66fdc339fdbbc56402ccf11fdeb16d35f
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-19 17:05:13 +0900

    init 'template'

--- index.js
+++ index.js
@@ -21,3 +21,32 @@ Modinit.prototype.create = function (res) {
     "license": res.license
   };
 };
+
+Modinit.prototype.template = function (prompt, modinitrc, options) {
+  var readme = _.template(fs.readFileSync('template/readme.markdown').toString());
+  var _package = _.template(fs.readFileSync('template/_package.json').toString());
+  var license = _.template(fs.readFileSync('template/LICENSE').toString());
+  var cmd = _.template(fs.readFileSync('template/cmd.js').toString());
+
+  var moduleName = _.slugify(prompt.moduleName);
+  var moduleVarName = _.camelize(prompt.moduleName);
+
+  readme({
+    'moduleName': moduleName,
+    'description': prompt.description,
+    'github': modinitrc.github,
+    'author': modinitrc.author
+  });
+  _package({
+    'moduleName': moduleName,
+    'description': prompt.description,
+    'github': modinitrc.github,
+    'author': modinitrc.author
+  });
+  license({
+    'author': modinitrc.author
+  });
+  cmd({
+    'moduleVarName': moduleVarName
+  });
+};
