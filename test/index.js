1ed32029a30c9fab792c37872f9243e96465a6a2
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-20 15:10:46 +0900

    tweak

--- test/index.js
+++ test/index.js
@@ -1,19 +1,21 @@
 
 var test = require('colored-tape');
 var Modinit = require('../');
+var modinit = new Modinit();
+
+var prompt = {
+  "github": "morishitter",
+  "author": "Masaaki Morishita",
+  "mail": "ikasama3.0@gmail.com",
+  "testfw": "tape",
+  "travis": "Y",
+  "readme": "readme.markdown",
+  "license": "MIT"
+};
+
+var modinitrc;
 
 test('create', function (t) {
-  var prompt = {
-    "github": "morishitter",
-    "author": "Masaaki Morishita",
-    "mail": "ikasama3.0@gmail.com",
-    "testfw": "tape",
-    "travis": "Y",
-    "readme": "readme.markdown",
-    "license": "MIT"
-  };
-
-  var modinit = new Modinit();
   var result = modinit.create(prompt);
 
   var expected = {
