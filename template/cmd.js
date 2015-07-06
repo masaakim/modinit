ffb5e7ff7ded643646da8c0afef6504c734459e9
Masaaki Morishita <ikasama3.0@gmail.com>
2015-07-06 22:35:57 +0900

    introduce minimist

--- template/cmd.js
+++ template/cmd.js
@@ -3,3 +3,28 @@
 var fs = require('fs')
 var pkg = require('../package.json')
 var <%= moduleName %> = require('..')
+
+var minimist = require('minimist')
+var argv = minimist(process.argv.slice(2), {
+    boolean: [
+        'help',
+        'versions'
+    ],
+    alias: {
+        h: 'help',
+        V: 'versions'
+    }
+})
+
+if (argv.V) {
+    console.log(pkg.version)
+}
+
+if (argv.h) {
+    console.log('Usage: atcss input-name output-name [options]');
+    console.log('');
+    console.log('Options:');
+    console.log('');
+    console.log('  -V, --versions    output the version number');
+    console.log('  -h, --help        output usage information');
+}
