b145c894a9a5b9a366b72f3a116ac894e3e94ca6
morishitter <morishitter@gmail.com>
2016-07-21 15:36:25 +0900

    fix test template file

--- template/test.js
+++ template/test.js
@@ -1,13 +1,6 @@
-var test = require('tape')
-var <%= moduleName %> = require('..')
-
-test('description', function (t) {
-    t.end()
-})
-
 import test from 'ava'
 import <%= moduleName %> from '../'
 
 test(t => {
-  
+
 })
